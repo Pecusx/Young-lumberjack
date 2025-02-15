d25780d710f17a4bfd5be22d52eaa377ad577e1a
pkali <pawel.kalinowski@gmail.com>
2025-02-15 13:08:17 -0500

    exporter updt

--- art/lumberjack_export.py
+++ art/lumberjack_export.py
@@ -1,29 +1,36 @@
 import json
 
-print('gamescreen_upper')
-with open('sky.atrview', 'rb') as f:
-    p = f.read()
-    pj=json.loads(p.decode('utf-8-sig'))
-dta = [pj['Pages'][0]['View'][i*80:(i+1)*80][:64] for i in range(19)]
-for l, d in enumerate(dta, 1):
-    print(f'l{l}')
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+def load_atrview(filename: str, page: int = 1) -> list[str]:
+    with open(filename, 'rb') as f:
+        dta = json.loads(f.read().decode('utf-8-sig'))['Pages'][page-1]['View']
+        return [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
 
+def print_lines(dta, line_from: int, line_to:int):
+    for d in dta[line_from:line_to]:
+        print(f'  dta ' + ','.join([f'${d[i:i + 2]}' for i in range(0, len(d), 2)]))
+
+
+print('gamescreen_upper')
+dta = load_atrview('sky.atrview')
+print("power_bar")
+print_lines(dta, 0,2)
+print("branch0")
+print_lines(dta, 2,7)
+print("branch1")
+print_lines(dta, 7,12)
+print("branch2")
+print_lines(dta, 12,17)
+print("branch3")
+print_lines(dta, 17,22)
+
+print()
 print('last_line_r')
-with open('phase1r.atrview', 'rb') as f:
-    p = f.read()
-    pj=json.loads(p.decode('utf-8-sig'))
-dta = pj['Pages'][0]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
+dta = load_atrview('phase1r.atrview')
 d = dta[-1]
 print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
 
 print('last_line_l')
-with open('phase1l.atrview', 'rb') as f:
-    p = f.read()
-    pj=json.loads(p.decode('utf-8-sig'))
-dta = pj['Pages'][0]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
+dta = load_atrview('phase1l.atrview')
 d = dta[-1]
 print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
 
@@ -31,88 +38,52 @@ print("""
     .align $100
 ; Right animation    
 gamescreen_lower1r  ; phase 1 page 1""")
-with open('phase1r.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][0]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
+dta = load_atrview('phase1r.atrview')
 for l, d in enumerate(dta[-9:-1], 1):
     print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
 
 print("gamescreen_lower2r  ; phase 2 page 1")
-with open('phase2r.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][0]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
+dta = load_atrview('phase2r.atrview')
 for l, d in enumerate(dta[-9:-1], 1):
     print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
 
 print('gamescreen_lower3r  ; phase 2 page 2')
-with open('phase2r.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][1]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase2r.atrview', page=2)
+print_lines(dta, -9, -1)
 
 print('gamescreen_lower4r  ; phase 2 page 3')
-with open('phase2r.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][2]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase2r.atrview', page=3)
+print_lines(dta, -9, -1)
 
 print('gamescreen_lower5r  ; phase 2 page 4')
-with open('phase2r.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][3]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase2r.atrview', page=4)
+print_lines(dta, -9, -1)
 
 print('gamescreen_lower6r  ; phase 3 page 1')
-with open('phase3r.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][0]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase3r.atrview', page=1)
+print_lines(dta, -9, -1)
 
 print('gamescreen_lower7r  ; phase 3 page 2')
-with open('phase3r.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][1]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase3r.atrview', page=2)
+print_lines(dta, -9, -1)
 
 print('gamescreen_lower8r  ; phase 3 page 3')
-with open('phase3r.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][2]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase3r.atrview', page=3)
+print_lines(dta, -9, -1)
 
 print('gamescreen_lower9r  ; phase 3 page 4')
-with open('phase3r.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][3]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase3r.atrview', page=4)
+print_lines(dta, -9, -1)
 
 print('gamescreen_lower10r  ; phase 3 page 5')
-with open('phase3r.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][4]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase3r.atrview', page=5)
+print_lines(dta, -9, -1)
 
 print("""
 ; left animation    
 gamescreen_lower1l  ; phase 1 page 1""")
+# pj = load_atrview('phase1l.atrview')
+# print_lines(dta, -9, -1)
 with open('phase1l.atrview', 'rb') as f:
     pj=json.loads(f.read().decode('utf-8-sig'))
 dta = pj['Pages'][0]['View']
@@ -121,73 +92,37 @@ for l, d in enumerate(dta[-9:-1], 1):
     print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
 
 print('gamescreen_lower2l  ; phase 2 page 1')
-with open('phase2l.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][0]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase2l.atrview', page=1)
+print_lines(dta, -9, -1)
 
 print('gamescreen_lower3l  ; phase 2 page 2')
-with open('phase2l.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][1]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase2l.atrview', page=2)
+print_lines(dta, -9, -1)
 
 print('gamescreen_lower4l  ; phase 2 page 3')
-with open('phase2l.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][2]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase2l.atrview', page=3)
+print_lines(dta, -9, -1)
 
 print('gamescreen_lower5l  ; phase 2 page 4')
-with open('phase2l.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][3]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase2l.atrview', page=4)
+print_lines(dta, -9, -1)
 
 print('gamescreen_lower6l  ; phase 3 page 1')
-with open('phase3l.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][0]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase3l.atrview', page=1)
+print_lines(dta, -9, -1)
 
 print('gamescreen_lower7l  ; phase 3 page 2')
-with open('phase3l.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][1]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase3l.atrview', page=2)
+print_lines(dta, -9, -1)
 
 print('gamescreen_lower8l  ; phase 3 page 3')
-with open('phase3l.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][2]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase3l.atrview', page=3)
+print_lines(dta, -9, -1)
 
 print('gamescreen_lower9l  ; phase 3 page 4')
-with open('phase3l.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][3]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase3l.atrview', page=4)
+print_lines(dta, -9, -1)
 
 print('gamescreen_lower10l  ; phase 3 page 5')
-with open('phase3l.atrview', 'rb') as f:
-    pj=json.loads(f.read().decode('utf-8-sig'))
-dta = pj['Pages'][4]['View']
-dta = [dta[i*80:(i+1)*80][:64] for i in range(len(dta)//80)]
-for l, d in enumerate(dta[-9:-1], 1):
-    print(f'  dta '+','.join([f'${d[i:i+2]}' for i in range(0, len(d), 2)]))
+dta = load_atrview('phase3l.atrview', page=5)
+print_lines(dta, -9, -1)
